67ab21ea1230a81b2c008dada87438fc78ade486
Svyatoslav <vdovichenko.fortech@gmail.com>
2022-11-08 17:25:30 +0300

    fix: fixed display price error

--- src/components/DisplayPrice/DisplayPrice.tsx
+++ src/components/DisplayPrice/DisplayPrice.tsx
@@ -1,21 +1,40 @@
 import { Typography } from "antd";
-import { FC } from "react"
+import { FC, useEffect } from "react";
 import { DeliveryMethod, priceDelivery, TypeDelivery } from "../../constants";
-import { IDelivery } from "../../types"
+import { IDelivery } from "../../types";
 
 interface IDisplayPrice {
-  values: IDelivery
+  values: IDelivery;
 }
 
 const DisplayPrice: FC<IDisplayPrice> = ({ values }) => {
-  let price = priceDelivery;
-  price *= DeliveryMethod[Number(values.deliveryMethod) - 1].deliveryTax;
-  price *= TypeDelivery[Number(values.rate) - 1].factor;
-  
+  const getCurrentPrice = () => {
+    let price = priceDelivery;
+
+    if (values.deliveryMethod && !values.rate) {
+      price *=
+        DeliveryMethod[Number(values.deliveryMethod) - 1].deliveryTax || 1;
+    }
+
+    if (values.rate && !values.deliveryMethod) {
+      price *= TypeDelivery[Number(values.rate) - 1].factor || 1;
+    }
+
+    if (values.deliveryMethod && values.rate) {
+      price *=
+        DeliveryMethod[Number(values.deliveryMethod) - 1].deliveryTax || 1;
+      price *= TypeDelivery[Number(values.rate) - 1].factor || 1;
+      return price;
+    }
+
+    return price
+  };
+
   return (
-    <Typography.Text>{`Текущая стоимость доставки: ${price} ₽`} </Typography.Text>
-  )
-}
+    <Typography.Text>
+      {`Текущая стоимость доставки: ${getCurrentPrice()} ₽`}{" "}
+    </Typography.Text>
+  );
+};
 
 export default DisplayPrice;
-
